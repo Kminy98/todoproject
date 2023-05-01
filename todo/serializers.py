2a47e2772df433b2a1e07ed5a7ce1175b57a82c8
kmin <pretty981210@gmail.com>
2023-05-01 13:59:16 +0900

    update

--- todo/serializers.py
+++ todo/serializers.py
@@ -4,7 +4,9 @@ from todo.models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = "__all__"
+        fields = ['id', 'user', 'title', 'is_complete', 'created_at', 'update_at', 'completion_at']
+        read_only_fields = ['user']
+        # fields = "__all__"
         # extra_kwargs = {
         #     "user": {
         #         # "requited":False,
